01cb92a393f23aa0dce69db3a14a14fcebcd354a
Saniokalex25 <41573628+Saniokalex25@users.noreply.github.com>
2018-07-25 16:49:07 +0300

    Update check.rb

--- check.rb
+++ check.rb
@@ -14,8 +14,8 @@ class Checking
   def checking(player)
     WINNING_LINES.each do |win|
     return true if win.all? {|winning| @values[winning]==player}
-    end
     false
+    end
   end
   def empty?
     return true if @values.all? {|cell| cell[1]!=" "}
