4c355a2e2f4495e8b7e75b8edb88290e95891fa3
Saniokalex25 <41573628+Saniokalex25@users.noreply.github.com>
2018-07-25 16:37:12 +0300

    Update tic-tac-toe.rb

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -2,24 +2,24 @@ require_relative 'engine_tic_tac'
 
 class Starting
   def start
-		while true
-			print "Do you want to play 2 person game(1), or with computer(2):"
-			@choose=gets.chop
-			if @choose=="1" || @choose=="2"
-				choose_game
-				break
-			end
-			puts "You had choosen bad number of move"
-		end
-	end
+    while true
+      print "Do you want to play 2 person game(1), or with computer(2):"
+      @choose=gets.chop
+      if @choose=="1" || @choose=="2"
+        choose_game
+        break
+      end
+      puts "You had choosen bad number of move"
+    end
+  end
 
   def choose_game
-		if @choose=="1"
+    if @choose=="1"
       Engine.new.two_player_start
-		elsif @choose=="2"
+    elsif @choose=="2"
       Engine.new.one_player_start
-		end
-	end
+    end
+  end
 end
 
 Starting.new.start
