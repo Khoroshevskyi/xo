fb64718c5647de3325b94b33a9bf8023b3502979
Saniokalex25 <41573628+Saniokalex25@users.noreply.github.com>
2018-07-25 17:01:04 +0300

    Update board.rb

--- board.rb
+++ board.rb
@@ -3,16 +3,16 @@ class Box
   def initialize(values)
     @values=values
     @whitespace=" "*6
-    @ramka="+-----------+"
+    @frame ="+-----------+"
   end
   def create_box
-    lala="#{@ramka}#{@whitespace}#{@ramka}\n"+
-	     "| #{@values["1"]} | #{@values["2"]} | #{@values["3"]} |#{@whitespace}| 1 | 2 | 3 |\n"+
-         "#{@ramka}#{@whitespace}#{@ramka}\n"+
-         "| #{@values["4"]} | #{@values["5"]} | #{@values["6"]} |#{@whitespace}| 4 | 5 | 6 |\n"+
-         "#{@ramka}#{@whitespace}#{@ramka}\n"+
-         "| #{@values["7"]} | #{@values["8"]} | #{@values["9"]} |#{@whitespace}| 7 | 8 | 9 |\n"+
-         "#{@ramka}#{@whitespace}#{@ramka}"
-    puts lala
+    complete_frame="#{@frame}#{@whitespace}#{@frame}\n"+
+		  "| #{@values["1"]} | #{@values["2"]} | #{@values["3"]} |#{@whitespace}| 1 | 2 | 3 |\n"+
+		  "#{@frame}#{@whitespace}#{@frame}\n"+
+		  "| #{@values["4"]} | #{@values["5"]} | #{@values["6"]} |#{@whitespace}| 4 | 5 | 6 |\n"+
+		  "#{@frame}#{@whitespace}#{@frame}\n"+
+		  "| #{@values["7"]} | #{@values["8"]} | #{@values["9"]} |#{@whitespace}| 7 | 8 | 9 |\n"+
+		  "#{@frame}#{@whitespace}#{@frame}"
+    puts complete_frame
   end
 end
