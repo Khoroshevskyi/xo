656d7b0cab96fa3c269948670898ee12ff06e065
Saniokalex25 <41573628+Saniokalex25@users.noreply.github.com>
2018-07-25 16:48:19 +0300

    Update check.rb

--- check.rb
+++ check.rb
@@ -1,26 +1,23 @@
 class Checking
+  attr_reader :values
 
-	attr_reader :values
+  WINNING_LINES = [
+    ["1","4", "7"], ["2", "5", "8"], ["3", "6", "9"], # Vertical lines
+    ["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"], # Horizontal lines
+    ["1", "5", "9"], ["3", "5", "7"] # Diagonals.
+    ]
+  
+  def initialize(values)
+    @values=values
+  end
 
-	WINNING_LINES = [
-		["1","4", "7"], ["2", "5", "8"], ["3", "6", "9"], # Vertical lines
-		["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"], # Horizontal lines
-		["1", "5", "9"], ["3", "5", "7"] # Diagonals.
-	]
-
-	def initialize(values)
-		@values=values
-
-	end
-
-	def checking(player)
-		WINNING_LINES.each do |win|
-		  return true if win.all? {|winning| @values[winning]==player}
-			false
-		end
-	end
-
-	def empty?
-		return true if @values.all? {|cell| cell[1]!=" "}
+  def checking(player)
+    WINNING_LINES.each do |win|
+    return true if win.all? {|winning| @values[winning]==player}
+    end
+    false
+  end
+  def empty?
+    return true if @values.all? {|cell| cell[1]!=" "}
   end
 end
