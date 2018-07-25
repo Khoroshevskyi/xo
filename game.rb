2d80ebef834cc050ec6b5b4b628f8fe6ec10c2cd
Saniokalex25 <41573628+Saniokalex25@users.noreply.github.com>
2018-07-25 16:43:58 +0300

    Update game.rb

--- game.rb
+++ game.rb
@@ -1,7 +1,6 @@
 require_relative 'engine_tic_tac'
 class Twoplayers
-
-	attr_reader :values, :player
+  attr_reader :values, :player
 
   def initialize(values, player)
     @values=values
@@ -12,17 +11,16 @@ class Twoplayers
       @sign="o"
     end
 	end
-
-	def player
-		print "#{@player} player step, choose number of cell: "
-		while true
-			s_player=gets.chop
-			if values[s_player]!="x" && values[s_player]!="o" && values.key?(s_player)
-				values[s_player]=@sign
-				break
-			else
-				 print "This number is not empty or is not in list, try again "
-			end
-		end
+  def player
+	print "#{@player} player step, choose number of cell: "
+    while true
+      s_player=gets.chop
+      if values[s_player]!="x" && values[s_player]!="o" && values.key?(s_player)
+        values[s_player]=@sign
+	    break
+	  else
+	    print "This number is not empty or is not in list, try again "
+      end
 	end
+  end
 end
